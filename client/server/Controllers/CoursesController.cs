05a444c8d57a451c3701a3b7f70d6905121a2a0f
Finoana Randri <110087713+FinoanaRandria@users.noreply.github.com>
2023-09-25 13:11:59 +0200

    creation get api

---
+++ client/server/Controllers/CoursesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+
+             return new List<string>() { "C#" , "SQL"};
+        }
+
+
+    }
+}
