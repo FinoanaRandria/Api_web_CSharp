a4d117519ac4ffb5df9590f3135fa8b911e8df93
Finoana Randri <110087713+FinoanaRandria@users.noreply.github.com>
2023-09-25 14:05:30 +0200

    connection string et migration

---
+++ client/server/Models/SchoolApiContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace server.Models
+{
+    public class SchoolApiContext : DbContext
+
+
+    {
+
+        public SchoolApiContext(DbContextOptions<SchoolApiContext> options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+    }
+}
--- client/server/Program.cs
+++ client/server/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using server.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+builder.Services.AddDbContext<SchoolApiContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolApiContextConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
